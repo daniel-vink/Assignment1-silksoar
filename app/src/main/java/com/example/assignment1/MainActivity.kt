5d522552cb9c93b43bd2edc8585bc5a284019031
daniel-vink <danielvink7@gmail.com>
2025-12-09 00:39:42 +0200

    Main screen to game screen

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.assignment1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+private lateinit var main_BTN_start: MaterialButton
+
+private lateinit var main_BTN_about: MaterialButton
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initViews()
+        findViews()
+    }
+
+    private fun initViews() {
+        main_BTN_start = findViewById(R.id.main_BTN_start)
+        main_BTN_about = findViewById(R.id.main_BTN_about)
+    }
+
+    private fun findViews() {
+        main_BTN_start.setOnClickListener { view -> changeView() }
+    }
+
+    private fun changeView() {
+        val intent = Intent(this, GameScreenActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
+
+
