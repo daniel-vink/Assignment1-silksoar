cfe1f5ca32f93c9e6c7887194b3120330f724d86
daniel-vink <danielvink7@gmail.com>
2025-12-14 23:50:28 +0200

    game - moving image test

--- app/src/main/java/com/example/assignment1/GameScreenActivity.kt
+++ app/src/main/java/com/example/assignment1/GameScreenActivity.kt
@@ -1,10 +1,20 @@
 package com.example.assignment1
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.ImageViewCompat
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+private lateinit var game_IMG_hornet: AppCompatImageView
+
+private lateinit var game_FAB_right: ExtendedFloatingActionButton
 
 class GameScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +26,22 @@ class GameScreenActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViews()
+        initViews()
+    }
+
+    private fun findViews() {
+        game_IMG_hornet = findViewById(R.id.game_IMG_hornet)
+        game_FAB_right = findViewById(R.id.game_FAB_right)
+    }
+
+    private fun initViews() {
+        game_FAB_right.setOnClickListener { View -> moveImage() }
+    }
+    private fun moveImage() {
+        game_IMG_hornet.animate()
+            .translationY(1000f)
+            .start()
     }
 }
